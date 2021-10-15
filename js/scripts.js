56c732d405c5574d14bf354aa60075a39e699136
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-10-15 19:38:58 +0300

    add gender code

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,36 @@
 
-function myFunction(){
+/*function myFunction(){
 	let yy=(document.getElementById("bday").value)
 	console.log(yy);
 
-let newDate=new Date(yy)
-console.log (newDate);
+   let newDate=new Date(yy)
+   console.log (newDate);
 
-let year=newDate.getFullYear();
-console.log(year);
+  let year=newDate.getFullYear();
+  console.log(year);
 
-let month=newDate.getMonth();
-console.log(month);
+  let month=newDate.getMonth();
+  console.log(month);
 
-let day=newDate.getDate();
-console.log(day);
+  let day=newDate.getDate();
+  console.log(day);
+
+  let exactDay=newDate.getDay()
+	console.log(exactDay);
 };
-
-
-
-
+*/
+function myGenderfunction(){
+	let female=(document.getElementById("female").value)
+	let male=(document.getElementById("male").value)
 	
 
+	if (female=true)
+	{
+		console.log(female);
+	}
+		else  {
 
+		console.log(male);
+		}
 
-
-
-
-
-
-	
-
-
-	
-	
+	};
