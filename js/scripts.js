143c17b5aa936dc949f17896d4a560d33005fcdc
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-10-15 21:17:12 +0300

    add arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-/*function myFunction(){
+function myFunction(){
 	let yy=(document.getElementById("bday").value)
 	console.log(yy);
 
@@ -18,16 +18,32 @@
   let exactDay=newDate.getDay()
 	console.log(exactDay);
 };
-*/
 
-function myGenderFunction(){
+	function myGenderFunction(){
 	
-let gender=	document.getElementById("female").checked
+		let gender=	document.getElementById("female").checked
+		
+		console.log(gender)
+		
+		let genderX=document.getElementById("male").checked
+		
+		console.log(genderX)
+		
+			};
+	let akanName=function akan(gender, exactDay, akanFemale, akanMale){
 
-console.log(gender)
-
-let genderX=document.getElementById("male").checked
-
-console.log(genderX)
-
+			akanFemale["Akosua","Adwoa","AbenaaAkua", "Yaa","Afua", "Ama"]
+			akanMale["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 	};
+	if (exactDay=0 && gender==="female")
+    	console.log(akanFemale[0])
+
+	
+	/*else if 
+		(exactDay=1 && gender==="female")
+	console.log(akanFemale[1])
+	
+	
+	else if 
+	(exactDay=2 && gender==="female")
+console.log(akanFemale[2])*/
