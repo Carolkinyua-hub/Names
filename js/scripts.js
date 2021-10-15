2b320bd18bb5d74e7614b30766660f04f6230a96
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-10-15 17:40:47 +0300

    add new javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,32 @@
 function myFunction(){
 	let yy=(document.getElementById("bday").value)
 	console.log(yy);
-	};
+
+let newDate=new Date()
+console.log newDate)
+
+let year=newdate.getyear()
+console.log(year)	
+
+let month=newdate.getmonth()
+console.log(month)
+let day=newdate.getday
+
+};
+
+
+
+
+	
+
+
+
+
+
+
+
+
+	
 
 
 	
